b69487ba8ac5144ff3cb95c62bb9b2426ed7b47d
Aaranyak Ghosh <68475438+aaranyak@users.noreply.github.com>
2020-08-19 14:07:47 +0530

    Create Input-output.py

---
+++ Handling_data/Input-output.py
@@ -0,0 +1,27 @@
+#Output in python
+
+#Create an outut statement that says whatever you like
+#This says Hi everybody
+
+print("Hi everybody")
+
+
+#Using Variables
+
+#A variable is a peice of data with a tag in it
+#A variable can be reffered to bu calling it's tag
+#Use the = sign to define a variable
+#It is defined by <tag> = <Data>
+
+#Examples
+
+number = 23
+
+word = "Puku"
+
+addition = number + 5
+
+sentence = "My name is Puku"
+
+#Input statements
+
